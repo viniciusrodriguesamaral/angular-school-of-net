70a37be73c00952d6448b7d6c01aa5250c2d47f3
= <vinicius@ferramentamestra.com.br>
2020-05-30 09:48:26 -0300

    Mudar employee delete para pasta components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeListComponent } from './components/employee/employee-list/employee-list.component';
 import {FormsModule} from '@angular/forms';
-import { SalaryColorDirective } from './salary-color.directive';
-import { EmployeeNewModalComponent } from './employee-new-modal/employee-new-modal.component';
-import { AlertSucessComponent } from './alert-sucess/alert-sucess.component';
-import { EmployeeEditModalComponent } from './employee-edit-modal/employee-edit-modal.component';
-import { EmployeeDeleteModalComponent } from './employee-delete-modal/employee-delete-modal.component';
-import { MyCurrencyPipe } from './my-currency.pipe';
+import { SalaryColorDirective } from './directives/salary-color.directive';
+import { EmployeeNewModalComponent } from './components/employee/employee-new-modal/employee-new-modal.component';
+import { AlertSucessComponent } from './components/alert-sucess/alert-sucess.component';
+import { EmployeeEditModalComponent } from './components/employee/employee-edit-modal/employee-edit-modal.component';
+import { EmployeeDeleteModalComponent } from './components/employee/employee-delete-modal/employee-delete-modal.component';
+import { MyCurrencyPipe } from './pipes/my-currency.pipe';
 
 @NgModule({
   declarations: [
